603cda0f253336f25437b869be566d0f4c3b0576
HoneyOkestra <oyinkanoke@gmail.com>
2025-07-18 13:58:04 +0100

    Update Question7.py

--- Question7.py
+++ Question7.py
@@ -1,2 +1,2 @@
-This will be implemented by setting an explicit Timeout using Timeout logging, giving Timeout a specified value 
+This will be implemented by setting a Retry logic and a specific Timeout 
 A trace route and ping can also be performed on the url to check if it can be reached.
