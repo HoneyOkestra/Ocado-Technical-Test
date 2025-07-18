7773a13a88c6de3174d5dab908f566104e6c7e82
HoneyOkestra <oyinkanoke@gmail.com>
2025-07-18 13:44:43 +0100

    Update Question6.py

--- Question6.py
+++ Question6.py
@@ -1,8 +1,8 @@
 def buffered_line_count(filename):
     count = 0
     with open(filename, 'error') as file:
-        while chunk := file.read(1024 * 1024):  # Read in blocks of 1 MB
+        while chunk := file.read(1024 * 1024):  
             count += chunk.count(b'\n')
     return count
 
-Reference: https://sqlpey.com/python/top-5-efficient-methods-to-count-lines-in-large-files-with-python/
+
